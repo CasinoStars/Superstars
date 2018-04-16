1a009a408bbb47a7833a95ab0128409f997bc217
Jeannin Franiçois <francoisjeannin9@gmail.com>
2018-04-16 15:27:28 +0200

    Auto stash before merge of "dev" and "origin/dev"
    adding element

---
+++ src/Superstars.WebApp/App/All'In/src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+  <header >
+      
+
+      <el-button >
+      trjcnzkcn
+      </el-button>
+
+  </header> 
+ </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style lang = "scss">
+
+</style>
