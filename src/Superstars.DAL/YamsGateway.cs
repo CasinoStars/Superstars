1b29c368c0c6df86522f1d096e036ecd7ede212e
Jeannin François <francoisjeannin9@gmail.com>
2018-04-17 15:19:16 +0200

    Start the Yams Game

---
+++ src/Superstars.DAL/YamsGateway.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Superstars.DAL
+{
+    class YamsGateway
+    {
+		readonly string _connectionString;
+
+		public YamsGateway(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public async Task<IEnumerable<YamsData>>GetClassement()
+		{
+			using (SqlConnection con = new SqlConnection(_connectionString))
+			{
+				return await con.QueryAsync<>(
+					
+					) 
+			}
+		}
+    }
+}
