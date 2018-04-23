6ac74c0afbad3410dec324e2bb6619af14e7031f
Max <clairson.max@gmail.com>
2018-04-23 18:55:45 +0200

    Auto stash before merge of "dev" and "origin/dev"
    bdd

---
+++ src/Superstars.DB/Scripts/20180411.014.Install-tElo.sql
@@ -0,0 +1,10 @@
+﻿create table sp.tElo
+(
+	GameTypeId int not null,
+	UserId int not null,
+	Elo int,
+
+	constraint FK_tGameTypeId foreign key (GameTypeId) references sp.tGameType(Gametype),
+	constraint FK_tUserId foreign key (UserId) references sp.tUser(UserId)
+
+)
