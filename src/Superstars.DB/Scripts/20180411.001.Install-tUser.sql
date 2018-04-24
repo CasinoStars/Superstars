e1eda28b7d5c4d72d7e883abe4b7346690118db2
MrMarceau <marceau.meignent@hotmail.com>
2018-04-24 14:43:15 +0200

    cs + bdd

--- src/Superstars.DB/Scripts/20180411.001.Install-tUser.sql
+++ src/Superstars.DB/Scripts/20180411.001.Install-tUser.sql
@@ -3,7 +3,7 @@ create table sp.tUser
 	UserId int identity(0,1),
 	UserName nvarchar(64) not null,
 	UserPassword varbinary(128) not null,
-	Email nvarchar(64) not null,
+	Email nvarchar(64) default '',
 	IsConnected bit default 0,
 	FakeMoney int default 0,
 	RealMoney int default 0,
@@ -11,6 +11,7 @@ create table sp.tUser
 	RealProfit int default 0,
 
 	constraint PK_tUser primary key(UserId),
-	constraint UK_tUser unique(UserName),
-	constraint UK_tUser unique(Email)	
-)
+	constraint UK_tUser_Name unique(UserName)
+);
+
+create unique index IX_tUser_Email on sp.tUser(Email) where Email <> '';
