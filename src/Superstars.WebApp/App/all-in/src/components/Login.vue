ca5470a99437aeb48e42f43472e6715b27ecc369
MrMarceau <marceau.meignent@hotmail.com>
2018-04-25 14:58:11 +0200

    view

---
+++ src/Superstars.WebApp/App/all-in/src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="home">
+    <h1>Bienvenue sur ALL-IN</h1>
+
+      <form id="login-form">
+        <input type="text" placeholder="Username" v-model="username" id="login"></input>
+        <input type="password" placeholder="Password" v-model="password" id="password"></input>
+        <button type="submit">Login</button>
+      </form>
+  </div>
+
+</template>
+
+<style lang="css">
+  
+#login-form {
+}
+
+#login {
+    margin: center;
+    border: 3px solid grey;
+  }
+
+#password {
+    margin: 120px;
+    border: 3px solid grey;
+}
+</style>
+
+
+  
