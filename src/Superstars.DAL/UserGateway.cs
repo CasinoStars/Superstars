386db763261f004652b2b1b2c4d22cee110d52da
MrMarceau <marceau.meignent@hotmail.com>
2018-04-26 10:10:00 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- src/Superstars.DAL/UserGateway.cs
+++ src/Superstars.DAL/UserGateway.cs
@@ -105,5 +105,24 @@ namespace Superstars.DAL
                     commandType: CommandType.StoredProcedure);
             }
         }
+
+        public async Task<Result<int>> IdentityVerify(string pseudo, byte[] password)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                var p = new DynamicParameters();
+                p.Add("@UserName", pseudo);
+                p.Add("@UserPassword", password);
+                p.Add("@UserId", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                p.Add("@Status", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+                await con.ExecuteAsync("sp.sUserVerify", p, commandType: CommandType.StoredProcedure);
+
+                int status = p.Get<int>("@Status");
+                if (status == 0) return Result.Failure<int>(Status.BadRequest, "Wrong Username or Password");
+
+                Debug.Assert(status == 1);
+                return Result.Success(Status.Created, p.Get<int>("@UserId"));
+            }
+        }
     }
 }
