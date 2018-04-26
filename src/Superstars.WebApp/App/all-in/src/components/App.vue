5c641c5a5334f2b324a1bab04b4c057b5323d359
Max <clairson.max@gmail.com>
2018-04-26 10:03:08 +0200

    App vue

--- src/Superstars.WebApp/App/all-in/src/components/App.vue
+++ src/Superstars.WebApp/App/all-in/src/components/App.vue
@@ -3,13 +3,42 @@
     <header>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <router-link class="navbar-brand" to="/">Superstars</router-link>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
+        <!-- <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/profil">Mon Profil</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/wallet">Mon Wallet</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/stats">Mes stats</router-link>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/logout">Déconnexion</router-link>
+          </li>
+          </ul>
+        </div> -->
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/register">Register</router-link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
-      <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -20,6 +49,7 @@ export default{
 </script>
 
 <style lang="scss" scoped>
+
 .progress {
   margin: 0px;
   padding: 0px;
