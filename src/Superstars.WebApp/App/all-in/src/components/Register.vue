ca2cb4422cb61950015777abf0509fa359f985c9
Max <clairson.max@gmail.com>
2018-04-29 18:19:18 +0200

    sorry bad register

--- src/Superstars.WebApp/App/all-in/src/components/Register.vue
+++ src/Superstars.WebApp/App/all-in/src/components/Register.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-
+ 
         <form @submit="onSubmit($event)">
             <!-- <div class="alert aler-danger" v-if="errors.length > 0">
                 <b>Les champs suivants sont invalides : </b>
@@ -8,17 +8,15 @@
                     <li v-for="e of errors">{{e}}</li>
                 </ul>
             </div>          -->
-
+ 
             <div class="form-group">
                 <label class="required">Pseudo : </label>
                 <input type="text" v-model="item.Pseudo" class="form-control" required>
-                <input type="text" v-model='item.Pseudo' class="form-control" required>
             </div>
-
+ 
         <div class="form-group">
             <label class="required">Mot de passe : </label>
             <input type="text" v-model="item.Password" class="form-control" required>
-            <input type="text" v-model='item.Password' class="form-control" required>
             <span asp-validation-for="Password"></span>
         </div>
             <button type="submit" class="btn btn-dark btn-block">S'inscrire</button>
@@ -29,7 +27,7 @@
 <script>
     import { mapActions } from 'vuex'
     import UserApiService from '../services/UserApiService'
-
+ 
     export default {
         data () {
             return {
@@ -39,19 +37,19 @@
         },
         methods: {
             ...mapActions(['executeAsyncRequest']),
-
+ 
             async onSubmit(e) {
                 e.preventDefault();
                 var errors = [];
-
+ 
                 if(!this.item.Pseudo) errors.push("Pseudo")
                 if(!this.item.Password) errors.push("Password")
-
+ 
                  this.errors = errors;
-
+ 
                 //if(errors.length == 0) {
                     try {
-                        await this.executeAsyncRequest(() => UserApiService.register(this.item));
+                        await this.executeAsyncRequest(() => UserApiService.registerAsync(this.item));
                         this.$router.replace('/');
                     }
                     catch(error) {
@@ -62,54 +60,7 @@
         }
     }
 </script>
-
+ 
 <style lang="scss">
-
+ 
 </style>
-
-    import UserApiService from '../services/UserApiService.js'
-    //import AuthService from '../services/UserApiService'
-
-    export default {
-         data () {
-             return {
-                 item: {},
-                 mode: null,
-                 id: null,
-                 errors: []
-             }
-         },
-
-         /*async mounted() {
-             this.mode = this.$route.params.mode;
-             this.id = this.$route.params.id;
-
-             try {
-                 this.item = await this.executeAsyncRequest(() => UserApiService.login());
-             }
-             catch(error) {
-                 this.$router.replace('/home');
-             }
-         },*/
-
-         methods: {
-             async onSubmit(e) {
-                 e.preventDefault();
-                 var errors = [];
-
-                 this.errors = errors;
-
-                 if(errors.length == 0) {
-                     try {
-                         console.log(this.item);
-                         await this.executeAsyncRequest(() => UserApiService.register(this.item));
-                         
-                         this.$router.replace('/home');
-                     }
-                     catch(error) {
-                     }
-                 }
-             }
-         }
-    }
-</script>
