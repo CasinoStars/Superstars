ba1e571cc9b00820d3ae635ace80688219222d0e
Max <clairson.max@gmail.com>
2018-04-29 18:04:00 +0200

    Little change package and register

--- src/Superstars.WebApp/App/all-in/src/components/Register.vue
+++ src/Superstars.WebApp/App/all-in/src/components/Register.vue
@@ -11,70 +11,56 @@
 
             <div class="form-group">
                 <label class="required">Pseudo : </label>
-                <input type="text" class="form-control" required>
+                <input type="text" v-model="item.Pseudo" class="form-control" required>
             </div>
 
         <div class="form-group">
             <label class="required">Mot de passe : </label>
-            <input type="text" class="form-control" required>
+            <input type="text" v-model="item.Password" class="form-control" required>
             <span asp-validation-for="Password"></span>
         </div>
-
-        <div class="form-group">
             <button type="submit" class="btn btn-dark btn-block">S'inscrire</button>
-        </div>
-        </form>
+    </form>
 </div>
   
 </template>
 <script>
     import { mapActions } from 'vuex'
-    //import AuthService from '../services/UserApiService'
+    import UserApiService from '../services/UserApiService'
 
     export default {
-    //     data () {
-    //         return {
-    //             item: {},
-    //             mode: null,
-    //             id: null,
-    //             errors: []
-    //         }
-    //     },
+        data () {
+            return {
+                item: {},
+                errors: []
+            }
+        },
+        methods: {
+            ...mapActions(['executeAsyncRequest']),
 
-    //     async mounted() {
-    //         this.mode = this.$route.params.mode;
-    //         this.id = this.$route.params.id;
+            async onSubmit(e) {
+                e.preventDefault();
+                var errors = [];
 
-    //         try {
-    //             this.item = await this.executeAsyncRequest(() => UserApiService.login());
-    //         }
-    //         catch(error) {
-    //             this.$router.replace('/home');
-    //         }
-    //     },
+                if(!this.item.Pseudo) errors.push("Pseudo")
+                if(!this.item.Password) errors.push("Password")
 
-    //     methods: {
-    //         //...mapActions(['executeAsyncRequest']),
+                 this.errors = errors;
 
-    //         async onSubmit(e) {
-    //             e.preventDefault();
-
-    //             var errors = [];
-
-    //             if(!this.item.Pseudo) errors.push("Pseudo")
-    //             if(!this.item.Password) errors.push("Password")
-
-    //             this.errors = errors;
-
-    //             if(errors.length == 0) {
-    //                 try {
-    //                     await this.executeAsyncRequest(() => UserApiService.register(this.item));
-    //                     this.$router.replace('/home');
-    //                 }
-    //                 catch(error) {
-    //                 }
-    //             }
-    //         }
-    //     }
+                //if(errors.length == 0) {
+                    try {
+                        await this.executeAsyncRequest(() => UserApiService.registerAsync(this.item));
+                        this.$router.replace('/');
+                    }
+                    catch(error) {
+                        this.$router.replace('/error');
+                    }
+                //}
+            }
+        }
     }
 </script>
+
+<style lang="scss">
+
+</style>
