5e310604e064588f1264fddb2faa701a14b70d21
Max <clairson.max@gmail.com>
2018-05-25 12:00:38 +0200

    fix sql error

--- src/Superstars.DB/Scripts/20180522.003.Install-sMoneyUpdate.sql
+++ src/Superstars.DB/Scripts/20180522.003.Install-sMoneyUpdate.sql
@@ -1,20 +1,20 @@
-create proc sp.sMoneyCreate
+create proc sp.sMoneyUpdate
 (
 	@MoneyId int out,
-	@MoneyType varchar,
+	@MoneyType varchar
 )
 as
 begin
 	set transaction isolation level serializable;
 	begin tran;
 
-	if not exists(select * from sp.tMoney m where m.[MoneyType] = @MoneyType
+	if not exists(select * from sp.tMoney m where m.MoneyType = @MoneyType)
 	begin
 		rollback;
 		return 1;
 	end;
 
-    update sp.tGameYams set [MoneyType] = @MoneyType where MoneyId = @MoneyId;
+    update sp.tMoney set MoneyType = @MoneyType where MoneyId = @MoneyId;
 	commit;
     return 0;
 end;
