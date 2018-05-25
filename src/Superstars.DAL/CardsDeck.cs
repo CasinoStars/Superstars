5ecadd6262b883d75f5d37bb78df435b87ea1bc9
MrMarceau <marceau.meignent@hotmail.com>
2018-05-25 14:38:11 +0200

    w

--- src/Superstars.DAL/CardsDeck.cs
+++ src/Superstars.DAL/CardsDeck.cs
@@ -6,33 +6,28 @@ namespace Superstars.DAL
 {
     class CardsDeck
     {
-        public Dictionary<string, int> Deck { get; set; }
+        public Dictionary<string, List<int>> Deck { get; set; }
 
-        public Dictionary<string, int> CreateDeck()
+        public void CreateDeck()
         {
-            string[] colors = new string[3];
+            Deck = new Dictionary<string, List<int>>();
+            string[] colors = new string[4];
             colors[0] = "Carreau";
             colors[1] = "Coeur";
             colors[2] = "Trefle";
             colors[3] = "Pique";
 
-            int[] numbers = new int[13];
-            for (int i = 1; i < 14; i++)
+            List<int> numbers = new List<int>(15);
+
+            for (int i = 2; i < numbers.Capacity; i++)
             {
-                numbers[i - 1] = i;
+                numbers.Add(i);
             }
 
-            Dictionary<string, int> deck = new Dictionary<string, int>();
-            foreach (string item in colors)
+            foreach (var item in colors)
             {
-                foreach (int nmbr in numbers)
-                {
-                    deck.Add(item, nmbr);
-                }
+                Deck.Add(item, numbers);
             }
-            return deck;
         }
-
-
     }
 }
