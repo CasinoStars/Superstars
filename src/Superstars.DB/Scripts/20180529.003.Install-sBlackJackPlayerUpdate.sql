9d05babf80d5439073be185aa9d5e77628ac7837
Jeannin François <francoisjeannin9@gmail.com>
2018-05-29 16:04:53 +0200

    BlackJack requests
    Table/View/Create/Update

---
+++ src/Superstars.DB/Scripts/20180529.003.Install-sBlackJackPlayerUpdate.sql
@@ -0,0 +1,27 @@
+create proc sp.sBlackJackPlayerUpdate
+(
+	@BlackJackPlayerId int out,
+	@BlackJackGameId int,
+	@PlayerCards nvarchar,
+)
+as
+begin
+	set transaction isolation level serializable;
+	begin tran;
+
+	     if not exists(select * from sp.tBlackJackGame bj where bj.[BlackJackPlayerId] = @BlackJackPlayerId
+	begin
+		rollback;
+		return 1;
+	end;
+
+	if exists(select * from sp.tBlackJackPlayer bj where bj.BlackJackPlayerId <> @BlackJackPlayerId and bj.BlackJackGameId = @BlackJackGameId and bj.PlayerCards = @PlayerCards) 
+	begin
+	   rollback
+	   return 2;
+	end;
+
+	    update sp.tBlackJackPlayer set [PlayerCards] = @PlayerCards where BlackJackPlayerId = @BlackJackPlayerId and BlackJackGameId = @BlackJackGameId;
+        commit;
+    return 0;
+end;
