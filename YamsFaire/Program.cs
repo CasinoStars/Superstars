d4c0b306988fd08a4cf3d3e6c5bd4daa8e596679
albin <albinmonsimier@gmail.com>
2018-06-05 16:24:15 +0200

    Auto stash before merge of "dev" and "origin/dev"
    WIP

---
+++ YamsFaire/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace YamsFaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dices dices = new Dices();
+            dices.rolleDices();
+
+            string ok = "oigroidjd";
+            SHA512 sha512 = new SHA512Managed();
+                Console.WriteLine(sha256.Hash);
+            sha512.ComputeHash(ok);
+
+            foreach (var dice in dices.dices)
+            {
+                Console.Write(dice + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ YamsFaire/dices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YamsFaire
+{
+    class Dices
+    {
+
+        public int[] dices { get; set; }
+
+        public Dices()
+        {
+            dices = new int[5] { 1, 1, 1, 1, 1 };
+        }
+
+
+        public int[] rolleDices()
+        {
+            Random random = new Random();
+            int i = 0;
+            foreach (var dice in dices)
+            {
+                dices[i] = random.Next(1, 6);
+                i++;
+            }
+           return dices;
+        }
+    }
+}
