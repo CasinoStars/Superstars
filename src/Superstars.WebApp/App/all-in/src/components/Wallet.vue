621f91694ccdf75c2034c1438401074da788a06b
Max <clairson.max@gmail.com>
2018-06-18 10:19:00 +0200

    upgrading vue wallet

--- src/Superstars.WebApp/App/all-in/src/components/Wallet.vue
+++ src/Superstars.WebApp/App/all-in/src/components/Wallet.vue
@@ -5,42 +5,48 @@
             <li v-for="e of errors" :key="e">{{e}}</li>
         </div>
 
-        <!-- change view wallet real/virutal !-->
-        <div style="text-align: center; margin-top: 2%;">
-            <div class="btn-group" v-if="this.wallet == 'fake'">
-                <button type="button" v-on:click="ChangeWallet('real')" class="btn btn-secondary">Real wallet</button>
-            </div>
-            <div class="btn-group" v-else>
-                <button type="button" v-on:click="ChangeWallet('fake')" class="btn btn-secondary">Fake wallet</button>
-            </div>
-        </div>
+        <!-- Upgrade -->
 
-        <!-- REALLY WALLET HERE -->
-        <div style="text-align: center;" v-if="this.wallet == 'real'">
-            <h1> Adresse de dépots BTC: {{BTCAddress}} </h1>
-            <h1>Solde du compte réel: {{trueCoins}}  BTC </h1>
-          <form @submit="Withdraw($event)">
-            <div class="form-group">
-                <label style="text-align: left;">Retrait :</label>
-                <input type="number" min="0" max="1000000" placeholder="Montant" v-model="item.AmountToSend" class="form-control" required>
-                <input type="text" placeholder="Address" v-model="item.DestinationAddress" class="form-control" required>
-            </div>
-            <button type="submit" class="btn btn-primary" @click="Withdraw" >Retrait</button>
-            </form>
-        </div>
+        <div class="form" style="letter-spacing: 2px; font-family: 'Courier New', sans-serif;">    
+            <ul class="tab-group">
+                <li style="color: white" class="tab active" v-if="this.wallet == 'real'"><a v-on:click="ChangeWallet('real')">Réel</a></li>
+                <li style="color: white" class="tab" v-else><a v-on:click="ChangeWallet('real')">Réel</a></li>
+                <li style="color: white" class="tab active" v-if="this.wallet == 'fake'"><a v-on:click="ChangeWallet('real')">Virtuel</a></li>
+                <li style="color: white" class="tab" v-else><a v-on:click="ChangeWallet('fake')">Virtuel</a></li>
+            </ul>
+      
+            <div class="tab-content">
+                <h5 style="color: white" v-if="this.wallet == 'real'"><span style="font-weight: bold; font-style: italic;">Solde du compte:</span><br>{{trueCoins}} BTC</h5>
+                <h5 style="color: white" v-if="this.wallet == 'real'"><span style="font-weight: bold; font-style: italic;">Adresse de dépots BTC:</span><br>{{BTCAddress}}<br><br></h5>
+                <h5 style="color: white" v-else><span style="font-weight: bold; font-style: italic;">Solde du compte:</span><br>{{fakeCoins.balance}} all'in</h5>
+                
+                <form  @submit="Withdraw($event)" v-if="this.wallet == 'real'">
+                        <div class="field-wrap">
+                            <label>
+                                Montant<span class="req">*</span>
+                            </label><br><br>
+                            <input  type="number" min="0" max="1000000" placeholder="Montant" v-model="item.AmountToSend" required autocomplete="off"/>
+                        </div>
+                    <div class="field-wrap">
+                        <label>
+                            Address<span class="req">*</span>
+                        </label><br><br>
+                        <input type="text" placeholder="Address" v-model="item.DestinationAddress" required autocomplete="off"/>
+                    </div><br>      
+                    <button type="submit" class="button button-block">Envoyer</button>
+                </form>
 
-        <!-- FAKE WALLET HERE -->
-        <div v-if="this.wallet == 'fake'">
-            <h1 style="text-align: center">Solde du compte virtuel: {{fakeCoins.balance}} all'in</h1>
-            <form @submit="onSubmit($event)" :v-model="item.moneyType = 2">
-            <div class="form-group">
-                <label style="text-align: left;">Montant :</label>
-                <input type="number" min="0" max="1000000" placeholder="..." v-model="item.fakeCoins" class="form-control" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Créditer</button>
-            </form>
-        </div>
-
+                <form @submit="onSubmit($event)" :v-model="item.moneyType = 2" v-else>
+                    <div class="field-wrap">
+                        <label>
+                            Montant<span class="req">*</span>
+                        </label><br><br>
+                        <input type="number" min="0" max="1000000" placeholder="..." v-model="item.fakeCoins" required autocomplete="off"/>
+                    </div><br>
+                    <button type="submit" class="button button-block">Créditer</button>
+                </form>
+        </div><!-- tab-content -->
+        </div> <!-- /form -->
     </div>
 </template>
 
@@ -126,3 +132,168 @@ export default {
 }
 </script>
 
+<style lang="scss">
+// @import "compass/css3";
+
+$body-bg: #c1bdba;
+$form-bg: #13232f;
+$white: #ffffff;
+
+$main: #777c7b;
+$main-light: lighten($main,5%);
+$main-dark: darken($main,5%);
+
+$gray-light: #a0b3b0;
+$gray: #ddd;
+
+$thin: 300;
+$normal: 400;
+$bold: 600;
+$br: 4px;
+
+// *, *:before, *:after {
+//   box-sizing: border-box;
+// }
+
+html {
+	overflow-y: scroll; 
+}
+
+body {
+  background:$body-bg;
+  font-family: 'Titillium Web', sans-serif;
+}
+
+a {
+  text-decoration:none;
+  color:$main;
+  transition:.5s ease;
+  &:hover {
+    color:$main-dark;
+  }
+}
+
+.form {
+  background:rgba($form-bg,.9);
+  padding: 40px;
+  max-width:600px;
+  margin:40px auto;
+  border-radius:$br;
+  box-shadow:0 4px 10px 4px rgba($form-bg,.3);
+}
+
+.tab-group {
+  list-style:none;
+  padding:0;
+  margin:0 0 40px 0;
+  &:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+  li a {
+    display:block;
+    text-decoration:none;
+    padding:15px;
+    background:rgba($gray-light,.25);
+    color:$gray-light;
+    font-size:20px;
+    float:left;
+    width:50%;
+    text-align:center;
+    cursor:pointer;
+    transition:.5s ease;
+    &:hover {
+      background:$main-dark;
+      color:$white;
+    }
+  }
+  .active a {
+    background:$main;
+    color:$white;
+  }
+}
+
+.tab-content > div:last-child {
+  display:none;
+}
+
+
+h5 {
+  text-align:center;
+  color:$white;
+  font-weight:$thin;
+}
+
+label {
+  position:absolute;
+  transform:translateY(6px);
+  left:13px;
+  color:rgba($white,.5);
+  transition:all 0.25s ease;
+  -webkit-backface-visibility: hidden;
+  pointer-events: none;
+  font-size:22px;
+  .req {
+  	margin:2px;
+  	color:#1ab188;;
+  }
+}
+
+input, textarea {
+  font-size:22px;
+  display:block;
+  width:100%;
+  height:100%;
+  padding:5px 10px;
+  background:none;
+  background-image:none;
+  border:1px solid $gray-light;
+  color:$white;
+  border-radius:0;
+  transition:border-color .25s ease, box-shadow .25s ease;
+  &:focus {
+		outline:0;
+		border-color:$main;
+  }
+}
+
+textarea {
+  border:2px solid $gray-light;
+  resize: vertical;
+}
+
+.field-wrap {
+  position:relative;
+  margin-bottom:10px;
+}
+
+
+.button {
+  border:0;
+  outline:none;
+  border-radius:0;
+  padding:15px 0;
+  font-size:2rem;
+  font-weight:$bold;
+  text-transform:uppercase;
+  letter-spacing:.1em;
+  background:$main;
+  color:$white;
+  transition:all.5s ease;
+  -webkit-appearance: none;
+  &:hover, &:focus {
+    background:$main-dark;
+  }
+}
+
+.button-block {
+  display:block;
+  width:100%;
+}
+
+.forgot {
+  margin-top:-20px;
+  text-align:right;
+}
+</style>
