26c95e1792a821fe56df35a0ec18d34f6b19d9a7
MrMarceau <marceau.meignent@hotmail.com>
2018-06-19 17:12:44 +0200

    cssssssssssss

--- src/Superstars.WebApp/App/all-in/src/components/MyStats.vue
+++ src/Superstars.WebApp/App/all-in/src/components/MyStats.vue
@@ -1,4 +1,5 @@
 <template>
+
 <div>
   <div style="text-align: center;margin-top 2%;font-family: 'Courier New', sans-serif;">
       <h1 style="text-decoration: underline;"> <strong> Mes statistiques : </strong></h1>
@@ -26,13 +27,29 @@
     <td>{{playernbgamesy}}</td>
   </tr>
 </table>
+
+<center>
+
+<div class="piecontainer">
+<canvas id="pie-chart" class="chartjs" width="770px" height="385px" ></canvas>
+</div>
+
+<div class="piecontainer">
+<canvas id="pie-chart2" class="chartjs" width="770px" height="385px" ></canvas>
+</div>
+
+</center>
+<!-- <canvas id="pie-chart" height="20px" width="40px"></canvas> -->
+
 </div>
 </template>
 
 <script>
+
 import { mapActions } from 'vuex';
 import Vue from 'vue';
 import GameApiService from '../services/GameApiService';
+import Chart from 'chart.js';
 
 export default {
       data() {
@@ -51,6 +68,43 @@ export default {
   async mounted() {
     await this.refreshBlackJackstats();
     await this.refreshYamsstats();
+
+new Chart(document.getElementById("pie-chart"), {
+    type: 'pie',
+    data: {
+      labels: ["Victoires", "Defaites"],
+      datasets: [{
+        label: "Ratio (percentage)",
+        backgroundColor: ["#3e95cd", "#8e5ea2"],
+        data: [this.playerwinsy,this.playerlossesy]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Ratio (%) for Yams'
+      }
+    }
+});
+
+new Chart(document.getElementById("pie-chart2"), {
+    type: 'pie',
+    data: {
+      labels: ["Victoires", "Defaites"],
+      datasets: [{
+        label: "Ratio (percentage)",
+        backgroundColor: ["#3e95cd", "#8e5ea2"],
+        data: [this.playerwinsbj,this.playerlossesbj]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Ratio (%) for BlackJack'
+      }
+    }
+});
+
   },
 
     
@@ -79,11 +133,22 @@ export default {
       }
     }
   }
+  
 }
 </script>
 
 
-<style>
+<style lang="scss">
+
+// #pie-chart {
+//    width: 10px;
+//    height: 10px;
+// }
+.piecontainer {
+height: 600px;
+width: 600px;
+display: inline-block;
+}
 
 table {
     font-family: arial, sans-serif;
