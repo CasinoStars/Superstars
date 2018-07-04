542010598208df76f685794ef122d72663771a8a
Max <clairson.max@gmail.com>
2018-07-05 00:56:13 +0200

    fix bad amount after game in BTCBankRoll

--- src/Superstars.WebApp/Controllers/WalletController.cs
+++ src/Superstars.WebApp/Controllers/WalletController.cs
@@ -72,7 +72,15 @@ namespace Superstars.WebApp.Controllers
             BitcoinSecret privateKey = new BitcoinSecret("cTSNviQWYnSDZKHvkjwE2a7sFW47sNoGhR8wjqVPb6RbwqH1pzup"); //PRIVATE KEY OF ALL'IN BANKROLL
             decimal onBlockchain = informationSeeker.HowMuchCoinInWallet(privateKey, new QBitNinjaClient(Network.TestNet)); //AMOUNT BTC ON BLOCKCHAIN
             Result<decimal> allCredit = await _walletGateway.GetAllCredit();
-            decimal BTCBank = onBlockchain + allCredit.Content;
+            decimal BTCBank;
+            if (allCredit.Content <= 0)
+            {
+                BTCBank = onBlockchain - allCredit.Content;
+            }
+            else
+            {
+                BTCBank = onBlockchain + allCredit.Content;
+            }
             return BTCBank;
         }
 
