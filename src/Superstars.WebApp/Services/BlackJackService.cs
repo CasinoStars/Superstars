5a8c0ab56d8774084639d67671de3358e14c8fb0
Max <clairson.max@gmail.com>
2018-10-17 18:47:02 +0200

    cleaning

--- src/Superstars.WebApp/Services/BlackJackService.cs
+++ src/Superstars.WebApp/Services/BlackJackService.cs
@@ -1,9 +1,5 @@
 ﻿using Superstars.WebApp.Controllers;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Superstars.WebApp
 {
--- src/Superstars.WebApp/Services/YamsService.cs
+++ src/Superstars.WebApp/Services/YamsService.cs
@@ -1,7 +1,5 @@
 ﻿using Superstars.DAL;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Superstars.WebApp.Services
