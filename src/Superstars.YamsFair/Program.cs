65b2ba4433a2ef11fd24b7589369410a05a89cf1
Hugo Loiseau <hloiseau@intechinfo.fr>
2018-10-19 15:26:35 +0200

    fix last merge

--- src/Superstars.YamsFair/Program.cs
+++ src/Superstars.YamsFair/Program.cs
@@ -8,6 +8,9 @@ namespace Superstars.YamsFair
         static void Main(string[] args)
         {
 
+
+            
+
             List<int> dicesFromSeeds = HashManager.RetriveDicesFromSeeds("ff0b463695f88ecd9a8946327b764a3a0acf935d1f6a527e9359643950bd025900495acd3c4a1e727f4ac5be9713e313700993878b0960a59a05627b9a51268b", "AlbinSeed",5);
 
             foreach (var item in dicesFromSeeds)
