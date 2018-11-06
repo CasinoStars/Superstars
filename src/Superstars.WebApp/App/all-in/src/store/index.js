b48aeab108624eebbba6479b8fe154e3eb48fa7c
Hugo Loiseau <hloiseau@intechinfo.fr>
2018-11-06 10:49:58 +0100

    added vue-beautiful-chat

--- src/Superstars.WebApp/App/all-in/src/store/index.js
+++ src/Superstars.WebApp/App/all-in/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Chat from 'vue-beautiful-chat'
 
 import * as actions from './actions';
 import * as getters from './getters';
@@ -7,6 +8,7 @@ import * as getters from './getters';
 import app from './modules/app';
 
 Vue.use(Vuex);
+Vue.use(Chat);
 
 const debug = process.env.NODE_ENV !== 'production';
 
