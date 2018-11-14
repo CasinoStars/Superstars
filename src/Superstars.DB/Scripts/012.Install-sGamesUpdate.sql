5f1ff13d4f974be47ee21fbe4040db5048ee96bd
MrMarceau <marceau.meignent@hotmail.com>
2018-11-14 19:59:17 +0100

    minor fix to run sql

--- src/Superstars.DB/Scripts/012.Install-sGamesUpdate.sql
+++ src/Superstars.DB/Scripts/012.Install-sGamesUpdate.sql
@@ -10,7 +10,7 @@ begin
        set transaction isolation level serializable;
        begin tran;
 
-       if not exists (select * from sp.tGames g where g.GameId = @GameId and g.GameTypeId = @GameTypeId and g.StartDate = @StartDate)
+       if not exists (select * from sp.tGames g where g.GameId = @GameId and g.GameTypeId = @GameTypeId)
        begin
              rollback;
              return 1;
