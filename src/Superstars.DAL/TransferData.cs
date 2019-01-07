b4f1283ec6c4795953de51e91b8b295cf5cee317
albin <albinmonsimier@gmail.com>
2019-01-07 16:10:46 +0100

    WIP

---
+++ src/Superstars.DAL/TransferData.cs
@@ -0,0 +1,19 @@
+﻿namespace Superstars.DAL
+{
+    public class ProvablyFairData
+    {
+        public string UncryptedPreviousServerSeed { get; set; }
+
+        public string UncryptedServerSeed { get; set; }
+
+        public string PreviousCryptedServerSeed { get; set; }
+
+        public string PreviousClientSeed { get; set; }
+
+        public string CryptedServerSeed { get; set; }
+
+        public string ClientSeed { get; set; }
+
+        public int Nonce { get; set; }
+    }
+}
---
+++ src/Superstars.DAL/TransferGateway.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Superstars.YamsFair;
+
+namespace Superstars.DAL
+{
+    public class TransferGateway
+    {
+        private readonly SqlConnexion _sqlConnexion;
+
+        public TransferGateway(SqlConnexion sqlConnection)
+        {
+            _sqlConnexion = sqlConnection;
+        }
+
+        public async Task<TransferData> GetTransferData(int userId)
+        {
+            using (var con = new SqlConnection(_sqlConnexion.connexionString))
+            {
+                return await con.QueryFirstOrDefaultAsync<TransferData>(
+                    "select m.TransferId, m.UserId, m.Amount, m.ReceiverId from sp.tProvablyFair m where m.UserId = @userId",
+                    new {UserId = userId});
+            }
+        }
+    }
+}
