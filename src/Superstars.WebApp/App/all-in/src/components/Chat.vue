2ccf258c86ecd8d0abce42c574ccb76202f199dd
Hugo Loiseau <hloiseau@intechinfo.fr>
2019-01-25 13:45:51 +0100

    color chat match site

--- src/Superstars.WebApp/App/all-in/src/components/Chat.vue
+++ src/Superstars.WebApp/App/all-in/src/components/Chat.vue
@@ -27,17 +27,17 @@
         isChatOpen: false, // to determine whether the chat window should be open or closed
         colors: {
           header: {
-            bg: '#4e8cff',
+            bg: '#373C42',
             text: '#ffffff'
           },
           launcher: {
-            bg: '#4e8cff'
+            bg: '#373C42'
           },
           messageList: {
             bg: '#ffffff'
           },
           sentMessage: {
-            bg: '#4e8cff',
+            bg: '#373C42',
             text: '#ffffff'
           },
           receivedMessage: {
@@ -45,7 +45,7 @@
             text: '#222222'
           },
           userInput: {
-            bg: '#f4f7f9',
+            bg: '#373C42',
             text: '#565867'
           }
         }, // specifies the color scheme for the component
